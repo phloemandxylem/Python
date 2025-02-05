b0dd0cff1f544e7e12c9e87710bc72e3a9669310
phloemandxylem <30699404+phloemandxylem@users.noreply.github.com>
2025-02-05 16:02:33 -0500

    Update pandas_data_project_week1.py

--- pandas_data_project_week1.py
+++ pandas_data_project_week1.py
@@ -3,8 +3,22 @@ df = pd.read_csv('foodhub_order.csv')
 # returns the first 5 rows
 df.head()
 
-## Checking the number of rows and columns 
+## checking the number of rows and columns 
 df.shape
 
-## Check the datatype of the csv
+## check the datatype of the csv
 df.info()
+
+## check the missing values
+missing_values=df.isnull().sum()
+print(missing_values)
+
+## checking statistical values in pandas
+min = df['food_preparation_time'].min()
+mean = df['food_preparation_time'].mean()
+max = df['food_preparation_time'].max()
+
+print('Minimum time: ', min)
+print('Average time: ', mean)
+print('Maximum time: ', max)
+
