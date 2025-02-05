cde7fe00799d6e03e680c0eee800ff959f86bd95
phloemandxylem <30699404+phloemandxylem@users.noreply.github.com>
2025-02-05 15:43:27 -0500

    Update and rename data_project_week1.py to pandas_data_project_week1.py

--- pandas_data_project_week1.py
+++ pandas_data_project_week1.py
@@ -1,7 +1,9 @@
-## reading a csv into a dataframe
+## reading a csv into a dataframe PANDAS
 df = pd.read_csv('foodhub_order.csv')
 # returns the first 5 rows
 df.head()
 
 ## Checking the number of rows and columns 
 df.shape
+
+## Check the datatype of the csv
