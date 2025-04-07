87279cad5153aac4ad0cb1c4a8198b1fbc25fb68
phloemandxylem <30699404+phloemandxylem@users.noreply.github.com>
2025-04-07 15:14:55 -0400

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,6 +1,24 @@
-# Concatenate strings
-introduction = "Hello!"
-doubleintro = introduction + introduction
-print(doubleintro)
+# Create a list, areas, that contains the area of the hallway (hall), kitchen (kit), living room (liv), bedroom (bed) and bathroom (bath), in this order. Use the predefined variables.
+hall = 11.25
+kit = 18.0
+liv = 20.0
+bed = 10.75
+bath = 9.50
 
-# 
+# Create list areas
+areas = [hall, kit, liv, bed, bath]
+
+# Print list areas
+print(areas)
+
+#Update the keys of a list areas
+areas = ["hallway", hall, "kitchen", kit, "living room", liv, "bedroom", bed, "bathroom", bath] 
+print(areas) 
+
+# Create a list of lists 
+house = [["hallway", hall], 
+         ["kitchen", kit],
+         ["living room", liv],
+         ["bedroom", bed], 
+         ["bathroom", bath]]
+print(house)
