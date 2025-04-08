9abae1bda20eeeccf8aef5ce997876a5771216dc
phloemandxylem <30699404+phloemandxylem@users.noreply.github.com>
2025-04-08 10:57:44 -0400

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,1 +1,15 @@
+# Basic functions
+# Use print() in combination with type() to print out the type of var1.
+var1 =  [ 1, 2, 3, 4]
+var2 = True
 
+print(type(var1))
+
+# Use len() to get the length of the list var1. Wrap it in a print() call to directly print it out.
+print(len(var1))
+
+# Use int() to convert var2 to an integer. Store the output as out2.
+out2 = int(var2)
+
+# Use Help
+help(pow())
