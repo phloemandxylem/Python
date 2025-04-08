ac1f005437e8c50260897ce117ca03966ac89d2d
phloemandxylem <30699404+phloemandxylem@users.noreply.github.com>
2025-04-08 11:23:21 -0400

    Update methods.py

--- methods.py
+++ methods.py
@@ -6,7 +6,10 @@ print(pier)
 print(pier_proper)
 print(pier.count("i"))
 
-#Use the .index() method to get the index of the element in areas that is equal to 20.0. Print out this index.
+#Use the .index() method to get the index of the element in colors that is equal to blue. Print out this index.
+colors = ["blue", "red", "yellow", "white" ] 
+print(areas.index("blue"))
 
-#Call .count() on areas to find out how many times 9.50 appears in the list. Again, simply print out this number.
+#Call .count() on areas to find out how many times "white" appears in the list. Again, simply print out this number.
+print(areas.count("white"))
 
