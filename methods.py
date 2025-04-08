e67b754a1d2709ad134ec67ed126db09df59a122
phloemandxylem <30699404+phloemandxylem@users.noreply.github.com>
2025-04-08 11:18:29 -0400

    Update methods.py

--- methods.py
+++ methods.py
@@ -1,1 +1,12 @@
+#Methods
+#Use the .upper() method on place and store the result in place_up. 
+pier = "pierspace"
+pier_proper = pier.upper()
+print(pier)
+print(pier_proper)
+print(pier.count("i"))
+
+#Use the .index() method to get the index of the element in areas that is equal to 20.0. Print out this index.
+
+#Call .count() on areas to find out how many times 9.50 appears in the list. Again, simply print out this number.
 
