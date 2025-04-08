67de85ca8ab53404e1117e68110d7de032520f83
phloemandxylem <30699404+phloemandxylem@users.noreply.github.com>
2025-04-08 11:57:34 -0400

    Update numpy.py

--- numpy.py
+++ numpy.py
@@ -1,1 +1,16 @@
+#Numpy
+#Import the numpy package as np, so that you can refer to numpy with np.
+import numpy as np
 
+#Create list baseball
+baseball = [180, 215, 210, 210, 188, 176, 209, 200]
+
+#Use np.array() to create a numpy array from baseball. Name this array np_baseball.
+np_bas3ball = np.array(baseball)
+
+#Print out the type of np_baseball to check that you got it right.
+print(type(np_bas3ball))
+
+#Converting lists
+#Create a numpy array from height_in. Name this new array np_height_in.
+np_height_in = np.array(height_in)
