85672ef79df243bcf478d3a6114988a3591623c7
phloemandxylem <30699404+phloemandxylem@users.noreply.github.com>
2025-04-08 11:41:08 -0400

    Update packages.py

--- packages.py
+++ packages.py
@@ -1,1 +1,15 @@
+#Packages
+#Import the math package
+import math
+
+#Use math.pi to calculate the circumference of the circle and store it in C.
+r = 0.43
+C = 2 * r * math.pi
+
+#Use math.pi to calculate the area of the circle and store it in A.
+A = math.pi * r ** 2
+
+#Print circumference and area
+print("Circumference: " + str(C))
+print("Area: " + str(A))
 
