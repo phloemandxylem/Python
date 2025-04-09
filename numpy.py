3f4c7a97fb668c0586d6975b99d68c00322fa3e5
phloemandxylem <30699404+phloemandxylem@users.noreply.github.com>
2025-04-09 11:44:38 -0400

    Update numpy.py

--- numpy.py
+++ numpy.py
@@ -2,7 +2,7 @@
 #Import the numpy package as np, so that you can refer to numpy with np.
 import numpy as np
 
-#Create list baseball
+#Create list baseball and convert to numpy array --------------------------
 baseball = [180, 215, 210, 210, 188, 176, 209, 200]
 
 #Use np.array() to create a numpy array from baseball. Name this array np_baseball.
@@ -11,11 +11,15 @@ np_bas3ball = np.array(baseball)
 #Print out the type of np_baseball to check that you got it right.
 print(type(np_bas3ball))
 
-#Converting lists
+
+#Converting numpy arrays -------------------------------------------------------
 #Create a numpy array from height_in. Name this new array np_height_in.
 height_in = [ 56, 48, 60, 52, 49 ]
 np_height_in = np.array(height_in)
 
 #Multiply np_height_in with 0.0254 to convert all height measurements from inches to meters. Store the new values in a new array, np_height_m.
 np_height_in_m = np_height_in * 0.0254
+print(np_height_in_m)
+
+
 
