b06017deb6fb398ff35fe9ad8a3cd08d0aa8687c
phloemandxylem <30699404+phloemandxylem@users.noreply.github.com>
2025-04-14 10:19:22 -0400

    Update numpy.py

--- numpy.py
+++ numpy.py
@@ -49,6 +49,16 @@ conversion = [0.0254, 0.453592, 1]
 #Print out product of np_baseball and conversion
 print(np_baseball*conversion)
 
+#Average versus median--------------------------------------------------------
+
+#Create np_height_in from np_baseball
+np_height_in = np_baseball[:,0]
+
+#Print out the mean of np_height_in
+print(np.mean(np_height_in))
+
+#Print out the median of np_height_in
+print(np.median(np_height_in))
 
 
 
