3054ddd81dc9ffeb2545b15f8f70db28839d45a8
phloemandxylem <30699404+phloemandxylem@users.noreply.github.com>
2025-04-18 12:04:40 -0400

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -26,6 +26,10 @@ homelessness_ind = homelessness.sort_values("individuals", ascending=False)
 # Print the top few rows
 print(homelessness_ind.head())
 
+# Print homelessness by region, then descending family members
+homelessness_reg_fam = homelessness.sort_values(["region", "family_members"], ascending=[True, False])
+
+print(homeless_reg_fam.head())
 
 
 
