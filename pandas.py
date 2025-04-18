f778fdb471bb4803759d66f70f6619a699d939fd
phloemandxylem <30699404+phloemandxylem@users.noreply.github.com>
2025-04-18 11:56:39 -0400

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -19,3 +19,11 @@ print(homelessness.column)
 
 # Print the row index of homelessness
 print(homelessness.index)
+
+# Sort homelessness by individuals
+homelessness_ind = homelessness.sort_values("individuals")
+
+# Print the top few rows
+print(homelessness_ind.head())
+
+
