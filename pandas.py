05095ee2e78564e07ebe58100d317fa6447c1a12
phloemandxylem <30699404+phloemandxylem@users.noreply.github.com>
2025-04-18 11:40:05 -0400

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -1,1 +1,9 @@
 # Exploring dataframes with Pandas-----------------------------------------------------
+# Print the head of data to expose the first few rows.
+print(homelessness.head())
+
+# Print information about the data showing columns, data types, and number of missing values.
+print(homelessness.shape)
+
+# Print a description of the data that calculates a few summary statistics for each column
+print(homelessness.describe())
